3c3a4c46a04c7c9bd156f1b27027e778550f1c80
bitrix <bitrix@example.com>
2025-10-11 23:31:08 +0000

    Initial commit

---
+++ .section.php
@@ -0,0 +1,9 @@
+<?
+$sSectionName = 'Главная';
+$arDirProperties = array(
+	'title' => 'Only Tasks',
+	'description' => 'Only Tasks',
+	'keywords' => 'trainee, tasks, only',
+	'robots' => 'index, follow'
+);
+?>
