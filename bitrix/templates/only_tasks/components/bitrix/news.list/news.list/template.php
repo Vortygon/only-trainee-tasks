736a20007f64e6f373e09836f6bf0b187a25f28a
Vortygon <50510432+Vortygon@users.noreply.github.com>
2025-10-12 08:07:56 +0700

    Обновить шаблон news.list

--- bitrix/templates/only_tasks/components/bitrix/news.list/news.list/template.php
+++ bitrix/templates/only_tasks/components/bitrix/news.list/news.list/template.php
@@ -23,7 +23,22 @@ $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID(
 $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
 ?>
 
-<article id="<?=$this->GetEditAreaId($arItem['ID']);?>">
+
+<div id="<?=$this->GetEditAreaId($arItem['ID']);?>">
+    <div class="article-list">
+      <a class="article-item article-list__item" href="<?=$arItem["DETAIL_PAGE_URL"]?>" data-anim="anim-3">
+        <div class="article-item__background">
+          <img src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>" data-src="xxxHTMLLINKxxx0.39186223192351520.41491856731872767xxx" alt=""/>
+        </div>
+        <div class="article-item__wrapper">
+            <div class="article-item__title"><?echo $arItem["NAME"];?></div>
+            <div class="article-item__content"><?echo $arItem["PREVIEW_TEXT"];?></div>
+        </div>
+      </a>
+    </div>
+</div>
+
+<!-- <article id="<?=$this->GetEditAreaId($arItem['ID']);?>">
 	
 	<header>
 		<?if($arParams["DISPLAY_NAME"]!="N" && $arParams["NAME"]):?>
@@ -72,7 +87,7 @@ $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayB
 		<?endforeach;?>
 	</section>
 
-</article>
+</article> -->
 
 <?endforeach;?>
 
