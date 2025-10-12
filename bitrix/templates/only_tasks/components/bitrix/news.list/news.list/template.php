6a17eacd9d571ebc52459c3aaa56496e4c4d5ae2
Vortygon <50510432+Vortygon@users.noreply.github.com>
2025-10-12 07:38:29 +0700

    Обновить шаблон news.list

--- bitrix/templates/only_tasks/components/bitrix/news.list/news.list/template.php
+++ bitrix/templates/only_tasks/components/bitrix/news.list/news.list/template.php
@@ -28,7 +28,7 @@ $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayB
 	<header>
 		<?if($arParams["DISPLAY_NAME"]!="N" && $arParams["NAME"]):?>
 			<?if(!$arParams["HIDE_LINK_WHEN_NO_DETAIL"] || ($arItem["DETAIL_TEXT"] && $arResult["USER_HAVE_ACCESS"])):?>
-				<h2><a href="<?=$arItem["DETAIL_PAGE_URL"]?>"><?=$arItem["NAME"]?></a></h2>
+				<a href="<?=$arItem["DETAIL_PAGE_URL"]?>"><h2><?=$arItem["NAME"]?></h2></a>
 			<?else:?>
 				<h2><?=$arItem["NAME"]?></h2>
 			<?endif?>
@@ -46,12 +46,12 @@ $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayB
 	</figure>
 
 	<section>
-		<?if($arParams["DISPLAY_PREVIEW_TEXT"]!="N" && $arItem["PREVIEW_TEXT"]):?>
-			<?echo $arItem["PREVIEW_TEXT"];?>
-		<?endif;?>
 		<?if($arParams["DISPLAY_DATE"]!="N" && $arItem["DISPLAY_ACTIVE_FROM"]):?>
 			<span class="news-date-time"><?echo $arItem["DISPLAY_ACTIVE_FROM"]?></span>
 		<?endif?>
+		<?if($arParams["DISPLAY_PREVIEW_TEXT"]!="N" && $arItem["PREVIEW_TEXT"]):?>
+			<?echo $arItem["PREVIEW_TEXT"];?>
+		<?endif;?>
 		<?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arItem["PREVIEW_PICTURE"])):?>
 			<div style="clear:both"></div>
 		<?endif?>
