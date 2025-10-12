79dde0a36aed609ad5271846a583cdb44db890d1
Vortygon <50510432+Vortygon@users.noreply.github.com>
2025-10-12 10:12:06 +0700

    Refactor

--- bitrix/templates/only_tasks/header.php
+++ bitrix/templates/only_tasks/header.php
@@ -10,8 +10,6 @@ if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
 		<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" /> 	
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta charset="utf-8">
-		<link rel="shortcut icon" href="images/favicon.604825ed.ico" type="image/x-icon">
-		<link href="css/common.css" rel="stylesheet">
 	</head>
 	<body>
 		<div id="panel">
