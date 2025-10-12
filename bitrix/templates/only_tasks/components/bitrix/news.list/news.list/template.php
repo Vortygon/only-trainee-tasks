ac9ef6fdda1017d2393974a3433cd82f1fa98549
Vortygon <50510432+Vortygon@users.noreply.github.com>
2025-10-12 08:16:00 +0700

    Обновить шаблон news.list

--- bitrix/templates/only_tasks/components/bitrix/news.list/news.list/template.php
+++ bitrix/templates/only_tasks/components/bitrix/news.list/news.list/template.php
@@ -13,87 +13,85 @@
 $this->setFrameMode(true);
 ?>
 <div class="news-list">
-<?if($arParams["DISPLAY_TOP_PAGER"]):?>
-	<?=$arResult["NAV_STRING"]?><br />
-<?endif;?>
+  <?if($arParams["DISPLAY_TOP_PAGER"]):?>
+    <?=$arResult["NAV_STRING"]?><br />
+  <?endif;?>
 
-<?foreach($arResult["ITEMS"] as $arItem):?>
-<?
-$this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
-$this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
-?>
+  <div id="<?=$this->GetEditAreaId($arItem['ID']);?>">
 
+    <?foreach($arResult["ITEMS"] as $arItem):?>
+    <?
+    $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+    $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+    ?>
 
-<div id="<?=$this->GetEditAreaId($arItem['ID']);?>">
-  <div id="barba-wrapper">
-    <div class="article-list">
-      <a class="article-item article-list__item" href="<?=$arItem["DETAIL_PAGE_URL"]?>" data-anim="anim-3">
-        <div class="article-item__background">
-          <img src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>" data-src="xxxHTMLLINKxxx0.39186223192351520.41491856731872767xxx" alt=""/>
-        </div>
-        <div class="article-item__wrapper">
-            <div class="article-item__title"><?echo $arItem["NAME"];?></div>
-            <div class="article-item__content"><?echo $arItem["PREVIEW_TEXT"];?></div>
-        </div>
-      </a>
-    </div>
+      <div class="article-list">
+        <a class="article-item article-list__item" href="<?=$arItem["DETAIL_PAGE_URL"]?>" data-anim="anim-3">
+          <div class="article-item__background">
+            <img src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>" data-src="xxxHTMLLINKxxx0.39186223192351520.41491856731872767xxx" alt=""/>
+          </div>
+          <div class="article-item__wrapper">
+              <div class="article-item__title"><?echo $arItem["NAME"];?></div>
+              <div class="article-item__content"><?echo $arItem["PREVIEW_TEXT"];?></div>
+          </div>
+        </a>
+      </div>
+
+    <!-- <article id="<?=$this->GetEditAreaId($arItem['ID']);?>">
+      
+      <header>
+        <?if($arParams["DISPLAY_NAME"]!="N" && $arParams["NAME"]):?>
+          <?if(!$arParams["HIDE_LINK_WHEN_NO_DETAIL"] || ($arItem["DETAIL_TEXT"] && $arResult["USER_HAVE_ACCESS"])):?>
+            <a href="<?=$arItem["DETAIL_PAGE_URL"]?>"><h3><?echo $arItem["NAME"];?></h3></a>
+          <?else:?>
+            <h3><?echo $arItem["NAME"];?></h3>
+          <?endif?>
+        <?endif?>
+      </header>
+      
+      <figure>
+        <?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arItem["PREVIEW_PICTURE"])):?>
+          <?if(!$arParams["HIDE_LINK_WHEN_NO_DETAIL"] || ($arItem["DETAIL_TEXT"] && $arResult["USER_HAVE_ACCESS"])):?>
+            <a href="<?=$arItem["DETAIL_PAGE_URL"]?>"><img class="preview-picture" border="0" src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>" width="<?=$arItem["PREVIEW_PICTURE"]["WIDTH"]?>" height="<?=$arItem["PREVIEW_PICTURE"]["HEIGHT"]?>" alt="<?=$arItem["NAME"]?>" hspace="0" vspace="2" title="<?=$arItem["NAME"]?>" style="float:left" /></a>
+          <?else:?>
+            <img class="preview-picture" border="0" src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>" width="<?=$arItem["PREVIEW_PICTURE"]["WIDTH"]?>" height="<?=$arItem["PREVIEW_PICTURE"]["HEIGHT"]?>" hspace="0" vspace="2" alt="<?=$arItem["NAME"]?>" title="<?=$arItem["NAME"]?>" style="float:left" />
+          <?endif;?>
+        <?endif?>
+      </figure>
+
+      <section>
+        <?if($arParams["DISPLAY_DATE"]!="N" && $arItem["DISPLAY_ACTIVE_FROM"]):?>
+          <span class="news-date-time"><?echo $arItem["DISPLAY_ACTIVE_FROM"]?></span>
+        <?endif?>
+        <?if($arParams["DISPLAY_PREVIEW_TEXT"]!="N" && $arItem["PREVIEW_TEXT"]):?>
+          <?echo $arItem["PREVIEW_TEXT"];?>
+        <?endif;?>
+        <?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arItem["PREVIEW_PICTURE"])):?>
+          <div style="clear:both"></div>
+        <?endif?>
+        <?foreach($arItem["FIELDS"] as $code=>$value):?>
+          <small>
+          <?=GetMessage("IBLOCK_FIELD_".$code)?>:&nbsp;<?=$value;?>
+          </small><br />
+        <?endforeach;?>
+        <?foreach($arItem["DISPLAY_PROPERTIES"] as $pid=>$arProperty):?>
+          <small>
+          <?=$arProperty["NAME"]?>:&nbsp;
+          <?if(is_array($arProperty["DISPLAY_VALUE"])):?>
+            <?=implode("&nbsp;/&nbsp;", $arProperty["DISPLAY_VALUE"]);?>
+          <?else:?>
+            <?=$arProperty["DISPLAY_VALUE"];?>
+          <?endif?>
+          </small><br />
+        <?endforeach;?>
+      </section>
+
+    </article> -->
+
+    <?endforeach;?>
+
+    <?if($arParams["DISPLAY_BOTTOM_PAGER"]):?>
+      <br /><?=$arResult["NAV_STRING"]?>
+    <?endif;?>
   </div>
-</div>
-
-<!-- <article id="<?=$this->GetEditAreaId($arItem['ID']);?>">
-	
-	<header>
-		<?if($arParams["DISPLAY_NAME"]!="N" && $arParams["NAME"]):?>
-			<?if(!$arParams["HIDE_LINK_WHEN_NO_DETAIL"] || ($arItem["DETAIL_TEXT"] && $arResult["USER_HAVE_ACCESS"])):?>
-				<a href="<?=$arItem["DETAIL_PAGE_URL"]?>"><h3><?echo $arItem["NAME"];?></h3></a>
-			<?else:?>
-				<h3><?echo $arItem["NAME"];?></h3>
-			<?endif?>
-		<?endif?>
-	</header>
-	
-	<figure>
-		<?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arItem["PREVIEW_PICTURE"])):?>
-			<?if(!$arParams["HIDE_LINK_WHEN_NO_DETAIL"] || ($arItem["DETAIL_TEXT"] && $arResult["USER_HAVE_ACCESS"])):?>
-				<a href="<?=$arItem["DETAIL_PAGE_URL"]?>"><img class="preview-picture" border="0" src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>" width="<?=$arItem["PREVIEW_PICTURE"]["WIDTH"]?>" height="<?=$arItem["PREVIEW_PICTURE"]["HEIGHT"]?>" alt="<?=$arItem["NAME"]?>" hspace="0" vspace="2" title="<?=$arItem["NAME"]?>" style="float:left" /></a>
-			<?else:?>
-				<img class="preview-picture" border="0" src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>" width="<?=$arItem["PREVIEW_PICTURE"]["WIDTH"]?>" height="<?=$arItem["PREVIEW_PICTURE"]["HEIGHT"]?>" hspace="0" vspace="2" alt="<?=$arItem["NAME"]?>" title="<?=$arItem["NAME"]?>" style="float:left" />
-			<?endif;?>
-		<?endif?>
-	</figure>
-
-	<section>
-		<?if($arParams["DISPLAY_DATE"]!="N" && $arItem["DISPLAY_ACTIVE_FROM"]):?>
-			<span class="news-date-time"><?echo $arItem["DISPLAY_ACTIVE_FROM"]?></span>
-		<?endif?>
-		<?if($arParams["DISPLAY_PREVIEW_TEXT"]!="N" && $arItem["PREVIEW_TEXT"]):?>
-			<?echo $arItem["PREVIEW_TEXT"];?>
-		<?endif;?>
-		<?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arItem["PREVIEW_PICTURE"])):?>
-			<div style="clear:both"></div>
-		<?endif?>
-		<?foreach($arItem["FIELDS"] as $code=>$value):?>
-			<small>
-			<?=GetMessage("IBLOCK_FIELD_".$code)?>:&nbsp;<?=$value;?>
-			</small><br />
-		<?endforeach;?>
-		<?foreach($arItem["DISPLAY_PROPERTIES"] as $pid=>$arProperty):?>
-			<small>
-			<?=$arProperty["NAME"]?>:&nbsp;
-			<?if(is_array($arProperty["DISPLAY_VALUE"])):?>
-				<?=implode("&nbsp;/&nbsp;", $arProperty["DISPLAY_VALUE"]);?>
-			<?else:?>
-				<?=$arProperty["DISPLAY_VALUE"];?>
-			<?endif?>
-			</small><br />
-		<?endforeach;?>
-	</section>
-
-</article> -->
-
-<?endforeach;?>
-
-<?if($arParams["DISPLAY_BOTTOM_PAGER"]):?>
-	<br /><?=$arResult["NAV_STRING"]?>
-<?endif;?>
 </div>
