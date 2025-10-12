49205f29d3d1fcd52b1925246942bff4850d8904
bitrix <bitrix@example.com>
2025-10-12 04:10:36 +0000

    Refactor

--- local/templates/only_tasks/components/bitrix/news.list/news.list/template.php
+++ local/templates/only_tasks/components/bitrix/news.list/news.list/template.php
@@ -1,23 +1,15 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
 /** @var array $arParams */
 /** @var array $arResult */
-/** @global CMain $APPLICATION */
-/** @global CUser $USER */
-/** @global CDatabase $DB */
-/** @var CBitrixComponentTemplate $this */
-/** @var string $templateName */
-/** @var string $templateFile */
-/** @var string $templateFolder */
-/** @var string $componentPath */
-/** @var CBitrixComponent $component */
 $this->setFrameMode(true);
 ?>
+
 <div class="news-list">
   <?if($arParams["DISPLAY_TOP_PAGER"]):?>
     <?=$arResult["NAV_STRING"]?><br />
   <?endif;?>
 
-  <div id="<?=$this->GetEditAreaId($arItem['ID']);?>">
+  <div id="barba-wrapper">
     <div class="article-list">
       <?foreach($arResult["ITEMS"] as $arItem):?>
 
@@ -29,7 +21,7 @@ $this->setFrameMode(true);
         <a class="article-item article-list__item" href="<?=$arItem["DETAIL_PAGE_URL"]?>" data-anim="anim-3" id="<?=$this->GetEditAreaId($arItem['ID']);?>">
           <div class="article-item__background">
             <?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arItem["PREVIEW_PICTURE"])):?>
-              <img src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>" data-src="xxxHTMLLINKxxx0.39186223192351520.41491856731872767xxx" alt=""/>
+              <img src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>" alt=""/>
             <?endif?>
           </div>
           <div class="article-item__wrapper">
