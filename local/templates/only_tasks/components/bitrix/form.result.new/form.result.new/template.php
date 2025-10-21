f176e4da02cb1f033514d7966b236c5c5c78ece4
Vortygon <50510432+Vortygon@users.noreply.github.com>
2025-10-21 18:27:03 +0700

    Обновить шаблон form.result.new

--- local/templates/only_tasks/components/bitrix/form.result.new/form.result.new/template.php
+++ local/templates/only_tasks/components/bitrix/form.result.new/form.result.new/template.php
@@ -20,9 +20,9 @@ if ($arResult['isFormNote'] != 'Y') {
             <?php endif; ?>
         </div>
         <form name="<?= $arResult['WEB_FORM_NAME'] ?>" class="contact-form__form" action="<?= POST_FORM_ACTION_URI ?>" method="POST">
-			<input type="hidden" name="WEB_FORM_ID" value="<?=$arParams['WEB_FORM_ID']?>">
+			<input type="hidden" name="WEB_FORM_ID" value="<?= $arParams['WEB_FORM_ID'] ?>">
 			<input type="hidden" name="web_form_submit" value="Y">
-			<?=bitrix_sessid_post()?>
+			<?= bitrix_sessid_post() ?>
             <div class="contact-form__form-inputs">
                 <?php
                 foreach ($arResult['QUESTIONS'] as $FIELD_SID => $arQuestion) {
@@ -56,44 +56,30 @@ if ($arResult['isFormNote'] != 'Y') {
                                         	',
                                             $arResult["QUESTIONS"][$FIELD_SID]['HTML_CODE']
                                         );
-                                        ?>
-                                        <!-- <input 
-                                            class="input__input" 
-                                            type="tel" 
-                                            id="<?= $FIELD_SID ?>" 
-                                            name="form" 
-                                            value=""
-                                            data-inputmask="'mask': '+79999999999', 'clearIncomplete': 'true'" 
-                                            maxlength="12" 
-                                            x-autocompletetype="phone-full"
-                                            <?= (intval($arQuestion['REQUIRED'] == 'Y') ? 'required=""' : '') ?>
-                                        > -->
-                                        <?php
                                         break;
                                     default:
                                         echo str_replace(
                                             '<input',
-                                            '<input class="input__input" ',
+                                            '<input class="input__input"',
                                             $arResult["QUESTIONS"][$FIELD_SID]['HTML_CODE']
                                         );
-                                        ?>
-                                        <!-- <input 
-                                            class="input__input" 
-                                            type="<?= $arQuestion['STRUCTURE'][0]['FIELD_TYPE'] ?>" 
-                                            id="<?= $FIELD_SID ?>" 
-                                            name="<?= $FIELD_SID ?>" 
-                                            value=""
-                                            <?= (intval($arQuestion['REQUIRED'] == 'Y') ? 'required=""' : '') ?>
-                                        > -->
-                                        <?php
                                         break;
                                 }
                                 ?>
-                                <?php if (isset($arResult['FORM_ERRORS'][$FIELD_SID])): ?>
-                                    <div class="input__notification">
-                                        <?= htmlspecialcharsbx($arResult['FORM_ERRORS'][$FIELD_SID]) ?>
-                                    </div>
-                                <?php endif; ?>
+                                <div class="input__notification">
+                                    <?php 
+                                    switch ($FIELD_SID) {
+                                        case 'medicine_phone':
+                                            break;
+                                        case 'medicine_email':
+                                            echo "Неверный формат почты";
+                                            break;
+                                        default:
+                                            echo "Поле должно содержать не менее 3-х символов";
+                                            break;
+                                    } 
+                                    ?>
+                                </div>
                             </label>
                         </div>
                         <?php
@@ -106,15 +92,13 @@ if ($arResult['isFormNote'] != 'Y') {
                     <label class="input__label">
                         <div class="input__label-text"><?= $arResult['QUESTIONS']['medicine_message']['CAPTION'] ?></div>
                         <?php
-                        $textareaHtml = $arResult['QUESTIONS']['medicine_message']['HTML_CODE'];
-                        $textareaHtml = str_replace('<textarea', '<textarea class="input__input"', $textareaHtml);
-                        echo $textareaHtml;
+                        echo str_replace(
+                            '<textarea',
+                            '<textarea class="input__input"',
+                            $arResult['QUESTIONS']['medicine_message']['HTML_CODE']
+                        );
                         ?>
-                        <?php if (isset($arResult['FORM_ERRORS']['medicine_message'])): ?>
-                            <div class="input__notification">
-                                <?= htmlspecialcharsbx($arResult['FORM_ERRORS']['medicine_message']) ?>
-                            </div>
-                        <?php endif; ?>
+                        <div class="input__notification"></div>
                     </label>
                 </div>
             </div>
