514f56b2b7e029e3d64128c8c4affb48e55f2694
Vortygon <50510432+Vortygon@users.noreply.github.com>
2025-11-11 10:22:43 +0700

    Фикс парсера

--- migrations/parser.php
+++ migrations/parser.php
@@ -45,7 +45,7 @@ if (($handle = fopen(CSV_FILE_NAME, 'r')) !== false) {
         foreach (['REQUIRE', 'DUTY', 'CONDITIONS'] as $key) {
             parseListValue($PROP[$key]);
         }
-        foreach (['ACTIVITY', 'FIELD', 'OFFICE', 'LOCATION', 'TYPE', 'SCHEDULE'] as $key) {
+        foreach (['ACTIVITY', 'FIELD', 'OFFICE', 'LOCATION', 'TYPE', 'SCHEDULE', 'SALARY_TYPE'] as $key) {
             handleDictionaryValue($key, $PROP[$key], $arrayProperties, $data[3]);
         }
         handleSalaryValue($PROP['SALARY_VALUE'], $PROP['SALARY_TYPE']);
@@ -163,5 +163,5 @@ function clearVacancies() {
         }
     }
     
-    echo "Удалено элементов: {$deletedCount}<br>";
+    echo "Удалено элементов: {$deletedCount}<br><br>";
 }
